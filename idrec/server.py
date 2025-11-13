ecc837d9a4bc7f2da24456972c3fa48fac8a660b
Giorgio Mariani <giorgiomariani94@gmail.com>
2025-11-13 15:53:26 +0100

    Update interface

--- idrec/server.py
+++ idrec/server.py
@@ -1,20 +1,37 @@
 import tempfile
 from typing import *
 from pathlib import Path
+from datetime import datetime
 
 import streamlit as st
 from PIL import Image
 from openai import OpenAI
-
+import pandas as pd
 
 CLIENT = OpenAI(api_key=st.secrets.openai_api_key)
 
 # Load config
 ALLOWED_EMAILS = st.secrets.allowed_emails
 
+NO_LABEL_CODE = "ERROR:NO-LABEL-FOUND"
+NO_TYPEFIELD_CODE = "ERROR:NO-TYPEFIELD-FOUND"
 
-USER_PROMPT = """
+
+USER_SYSTEM_PROMPT = "You are a VQA assistant, you help solve visual question-answering tasks provided by the user. Your answer are minimal, providing only the requested information."
+USER_PROMPT = f"""
 Answer with only the code on the field 'type' in the label. Do not include additional text in your reply.
+If the image does not contain a label: answer with '{NO_LABEL_CODE}'.
+If the image does not contain a 'TYPE' field: answer with '{NO_TYPEFIELD_CODE}'.
+
+Examples:
+> user: <image>
+  assistant: A1B-C123-ABC
+
+> user: <image without label>
+  assistant: {NO_LABEL_CODE}
+
+> user: <image with a label without the TYPE field>
+  assistant: {NO_TYPEFIELD_CODE}
 """
 
 
@@ -63,38 +80,58 @@ def send_for_annotation(image: Image.Image):
         return f"⚠️ Request failed: {e.with_traceback(None)}"
 
 
-if not st.user.is_logged_in:
-    if st.button("Log in"):
+def login_page():
+    st.title("Pagina di Log-In")
+    if st.button("Accedi al tuo account Google ", icon=":material/login:"):
         st.login()
-    st.stop()
-else:
-    user_email = st.user.email  # or appropriate attribute
+        st.session_state.codes = pd.DataFrame(columns=["PRODUCT_ID", "TIMESTAMP"])
 
-    if user_email not in ALLOWED_EMAILS:
-        st.error("Access denied: email not whitelisted.")
+
+def main():
+    if not st.user.is_logged_in:
+        login_page()
         st.stop()
+    else:
+        if st.user.email not in ALLOWED_EMAILS:
+            st.error(
+                f"Accesso Negato: L'account con email {st.user.email} non è stato autorizzato ad accedere all'applicazione."
+            )
+            st.stop()
 
-st.set_page_config(page_title="Phone Camera Annotation App")
+    st.set_page_config(page_title="Annotation App")
+    st.title("Extrazione codici OMRON")
+    st.write("Questa è un app per l'estrazione automatica del codice di prodotti elettronici mandati da Omron Italia.")
 
-st.title("📸 Phone Camera Annotation App")
-st.write("Take a photo with your phone and send it to the annotation server.")
+    uploaded_image = st.camera_input("Fai una foto")
+
+    if uploaded_image is not None:
+        df = st.session_state.codes
+        if st.button("Invia immagine al server."):
+            image = Image.open(uploaded_image)
+
+            # Send POST request
+            with st.spinner("L'immagine sta venendo processata..."):
+                with tempfile.NamedTemporaryFile(suffix=".jpg") as fp:
+                    image.save(fp)
+                    response = request_id(CLIENT, fp.name)
+
+            if response == NO_LABEL_CODE:
+                st.warning("L'immagine non contiene un'etichetta.")
+            elif response == NO_TYPEFIELD_CODE:
+                st.warning("L'etichetta nell'immagine non dispone di un campo 'TYPE'.")
+            else:
+                st.markdown(response.output_text)
+
+                # Update dataframe
+                timestamp = datetime.today().strftime("%Y-%m-%d_%H%M")
+                df.loc[len(df)] = (response, timestamp)
+
+        # Show dataframe
+        st.dataframe(df)
+        st.download_button(data=df)
+    else:
+        st.info("Fare una foto per scansionare e processare l'etichetta.")
 
 
-uploaded_image = st.camera_input("Take a photo")
-
-if uploaded_image is not None:
-    if st.button("Send to Annotation Server"):
-        # Convert image to bytes
-        image = Image.open(uploaded_image)
-
-        # Send POST request
-        with st.spinner("Sending image to server..."):
-            with tempfile.NamedTemporaryFile(suffix=".jpg") as fp:
-
-                image.save(fp)
-                response = request_id(CLIENT, fp.name)
-
-        st.markdown(response.output_text)
-
-else:
-    st.info("Please take a photo using your phone camera above.")
+if __name__ == "__main__":
+    main()
